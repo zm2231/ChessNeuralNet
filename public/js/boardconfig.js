efd71257fdc154401a725ea2344daaacfb4e5749
Zain Merchant <merchantzains@gmail.com>
2019-11-21 20:37:16 -0500

    might revert later, something got messed up with evaluations being put in

--- public/js/boardconfig.js
+++ public/js/boardconfig.js
@@ -1,16 +1,21 @@
+let Chess = require('./chess.min.js');
+// let ChessBoard = require('./chessboard-0.3.0.min.js');
+
 var board,
-    game = new Chess();
+  game = new Chess();
+
+  console.log()
 
 // Actions after any move
 var onMoveEnd = function(oldPos, newPos) {
   // Alert if game is over
   if (game.game_over() === true) {
-    //alert('Game Over');
+    alert('Game Over');
     console.log('Game Over');
   }
 
   // Log the current game position
-  console.log(game.fen());
+  // console.log(game.fen());
 };
 
 // Check before pick pieces that it is white and game is not over
@@ -26,6 +31,28 @@ var onSnapEnd = function() {
   board.position(game.fen());
 };
 
+// Handles what to do after human makes move.
+// Computer automatically makes next move
+var onDrop = function(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
+
+  // If illegal move, snapback
+  if (move === null) return 'snapback';
+
+  // Log the move
+  // console.log(move);
+
+  // make move for black
+  window.setTimeout(function() {
+    makeMove(4, 3);
+  }, 250);
+};
+
 // Configure board
 var cfg = {
   draggable: true,
@@ -35,5 +62,8 @@ var cfg = {
   onDragStart: onDragStart,
   onDrop: onDrop,
   onSnapEnd: onSnapEnd
-}
-board = ChessBoard('board', cfg);
+};
+
+// board = ChessBoard('board', cfg);
+
+// module.exports = board;
--- public/js/movecalc.js
+++ public/js/movecalc.js
@@ -13,15 +13,15 @@ var randomMove = function() {
  * @param {string} color - Players color, either 'b' or 'w'
  * @return {Number} board value relative to player
  */
-var eval_1 = function(board, color) {
+var evaluateBoard = function(board, color) {
   // Sets the value for each piece using standard piece value
   var pieceValue = {
-    'p': 100,
-    'n': 350,
-    'b': 350,
-    'r': 525,
-    'q': 1000,
-    'k': 10000
+    p: 100,
+    n: 350,
+    b: 350,
+    r: 525,
+    q: 1000,
+    k: 10000
   };
 
   // Loop through all pieces on the board and sum up total
@@ -30,8 +30,8 @@ var eval_1 = function(board, color) {
     row.forEach(function(piece) {
       if (piece) {
         // Subtract piece value if it is opponent's piece
-        value += pieceValue[piece['type']]
-                 * (piece['color'] === color ? 1 : -1);
+        value +=
+          pieceValue[piece['type']] * (piece['color'] === color ? 1 : -1);
       }
     });
   });
@@ -40,8 +40,108 @@ var eval_1 = function(board, color) {
 };
 
 /**
+ * Calculates the best move looking one move ahead
+ * @param {string} playerColor - Players color, either 'b' or 'w'
+ * @return {string} the best move
+ */
+var calcBestMoveOne = function(playerColor) {
+  // List all possible moves
+  var possibleMoves = game.moves();
+  // Sort moves randomly, so the same move isn't always picked on ties
+  possibleMoves.sort(function(a, b) {
+    return 0.5 - Math.random();
+  });
+
+  // exit if the game is over
+  if (game.game_over() === true || possibleMoves.length === 0) return;
+
+  // Search for move with highest value
+  var bestMoveSoFar = null;
+  var bestMoveValue = Number.NEGATIVE_INFINITY;
+  possibleMoves.forEach(function(move) {
+    game.move(move);
+    var moveValue = evaluateBoard(game.board(), playerColor);
+    if (moveValue > bestMoveValue) {
+      bestMoveSoFar = move;
+      bestMoveValue = moveValue;
+    }
+    game.undo();
+  });
+
+  return bestMoveSoFar;
+};
+
+/**
+ * Calculates the best move using Minimax without Alpha Beta Pruning.
+ * @param {Number} depth - How many moves ahead to evaluate
+ * @param {Object} game - The game to evaluate
+ * @param {string} playerColor - Players color, either 'b' or 'w'
+ * @param {Boolean} isMaximizingPlayer - If current turn is maximizing or minimizing player
+ * @return {Array} The best move value, and the best move
+ */
+var calcBestMoveNoAB = function(
+  depth,
+  game,
+  playerColor,
+  isMaximizingPlayer = true
+) {
+  // Base case: evaluate board
+  if (depth === 0) {
+    value = evaluateBoard(game.board(), playerColor);
+    return [value, null];
+  }
+
+  // Recursive case: search possible moves
+  var bestMove = null; // best move not set yet
+  var possibleMoves = game.moves();
+  // Set random order for possible moves
+  possibleMoves.sort(function(a, b) {
+    return 0.5 - Math.random();
+  });
+  // Set a default best move value
+  var bestMoveValue = isMaximizingPlayer
+    ? Number.NEGATIVE_INFINITY
+    : Number.POSITIVE_INFINITY;
+  // Search through all possible moves
+  for (var i = 0; i < possibleMoves.length; i++) {
+    var move = possibleMoves[i];
+    // Make the move, but undo before exiting loop
+    game.move(move);
+    // Recursively get the value of this move
+    value = calcBestMoveNoAB(
+      depth - 1,
+      game,
+      playerColor,
+      !isMaximizingPlayer
+    )[0];
+    // Log the value of this move
+    // console.log(isMaximizingPlayer ? 'Max: ' : 'Min: ', depth, move, value,
+    // bestMove, bestMoveValue);
+
+    if (isMaximizingPlayer) {
+      // Look for moves that maximize position
+      if (value > bestMoveValue) {
+        bestMoveValue = value;
+        bestMove = move;
+      }
+    } else {
+      // Look for moves that minimize position
+      if (value < bestMoveValue) {
+        bestMoveValue = value;
+        bestMove = move;
+      }
+    }
+    // Undo previous move
+    game.undo();
+  }
+  // Log the best move at the current depth
+  // console.log('Depth: ' + depth + ' | Best Move: ' + bestMove + ' | ' + bestMoveValue);
+  // Return the best move, or the only move
+  return [bestMoveValue, bestMove || possibleMoves[0]];
+};
+
+/**
  * Calculates the best move using Minimax with Alpha Beta Pruning.
- * @param {function} eval - The eval function to be used
  * @param {Number} depth - How many moves ahead to evaluate
  * @param {Object} game - The game to evaluate
  * @param {string} playerColor - Players color, either 'b' or 'w'
@@ -50,34 +150,48 @@ var eval_1 = function(board, color) {
  * @param {Boolean} isMaximizingPlayer - If current turn is maximizing or minimizing player
  * @return {Array} The best move value, and the best move
  */
-var calcBestMove = function(eval, depth, game, playerColor,
-                            alpha=Number.NEGATIVE_INFINITY,
-                            beta=Number.POSITIVE_INFINITY,
-                            isMaximizingPlayer=true) {
+var calcBestMove = function(
+  depth,
+  game,
+  playerColor,
+  alpha = Number.NEGATIVE_INFINITY,
+  beta = Number.POSITIVE_INFINITY,
+  isMaximizingPlayer = true
+) {
   // Base case: evaluate board
   if (depth === 0) {
-    value = eval(game.board(), playerColor);
-    return [value, null]
+    value = evaluateBoard(game.board(), playerColor);
+    return [value, null];
   }
 
   // Recursive case: search possible moves
   var bestMove = null; // best move not set yet
   var possibleMoves = game.moves();
   // Set random order for possible moves
-  possibleMoves.sort(function(a, b){return 0.5 - Math.random()});
+  possibleMoves.sort(function(a, b) {
+    return 0.5 - Math.random();
+  });
   // Set a default best move value
-  var bestMoveValue = isMaximizingPlayer ? Number.NEGATIVE_INFINITY
-                                         : Number.POSITIVE_INFINITY;
+  var bestMoveValue = isMaximizingPlayer
+    ? Number.NEGATIVE_INFINITY
+    : Number.POSITIVE_INFINITY;
   // Search through all possible moves
   for (var i = 0; i < possibleMoves.length; i++) {
     var move = possibleMoves[i];
     // Make the move, but undo before exiting loop
     game.move(move);
     // Recursively get the value from this move
-    value = calcBestMove(eval, depth-1, game, playerColor, alpha, beta, !isMaximizingPlayer)[0];
+    value = calcBestMove(
+      depth - 1,
+      game,
+      playerColor,
+      alpha,
+      beta,
+      !isMaximizingPlayer
+    )[0];
     // Log the value of this move
-    console.log(isMaximizingPlayer ? 'Max: ' : 'Min: ', depth, move, value,
-                bestMove, bestMoveValue);
+    // console.log(isMaximizingPlayer ? 'Max: ' : 'Min: ', depth, move, value,
+    // bestMove, bestMoveValue);
 
     if (isMaximizingPlayer) {
       // Look for moves that maximize position
@@ -98,12 +212,12 @@ var calcBestMove = function(eval, depth, game, playerColor,
     game.undo();
     // Check for alpha beta pruning
     if (beta <= alpha) {
-      console.log('Prune', alpha, beta);
+      // console.log('Prune', alpha, beta);
       break;
     }
   }
   // Log the best move at the current depth
-  console.log('Depth: ' + depth + ' | Best Move: ' + bestMove + ' | ' + bestMoveValue + ' | A: ' + alpha + ' | B: ' + beta);
+  // console.log('Depth: ' + depth + ' | Best Move: ' + bestMove + ' | ' + bestMoveValue + ' | A: ' + alpha + ' | B: ' + beta);
   // Return the best move, or the only move
   return [bestMoveValue, bestMove || possibleMoves[0]];
-}
+};
